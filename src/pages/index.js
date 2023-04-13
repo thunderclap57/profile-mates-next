3521cf2336a521b447db857ab385ffb95ec5596f
zenitsu <zenitsu@pop-os.localdomain>
2023-04-13 19:05:22 +0530

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -55,77 +55,7 @@ export default function Home() {
           <HowTo />
           <Facts />
           <Footer />
-          <UserDetails
-            user={{
-              userId: "12345",
-              name: "John Doe",
-              email: "johndoe@example.com",
-              phone: 1234567890,
-              dob: "01/01/1990",
-              image: "https://randomuser.me/api/portraits/men/1.jpg",
-              awards: "Best Employee of the Month",
-              linkedIn: "https://www.linkedin.com/in/johndoe/",
-              certifications: "Certified Scrum Master",
-              gitHub: "https://github.com/johndoe",
-              hackerRank: "https://www.hackerrank.com/johndoe",
-              skills: {
-                skill1: "JavaScript",
-                skill2: "React",
-                skill3: "Node.js",
-                skill4: "HTML",
-                skill5: "CSS",
-                skill6: "MongoDB",
-              },
-              education: {
-                Primary: {
-                  Institution_name: "St. Mary's High School",
-                  Year_of_pass: "2008",
-                  Score: "90%",
-                },
-                Higher: {
-                  Institution_name: "St. Xavier's College",
-                  Year_of_pass: "2010",
-                  Score: "85%",
-                },
-                UG: {
-                  Institution_name: "University of Mumbai",
-                  Year_of_pass: "2013",
-                  Score: "80%",
-                },
-                PG: {
-                  Institution_name: "University of Pune",
-                  Year_of_pass: "2015",
-                  Score: "75%",
-                },
-              },
-              languages_known: {
-                language1: "Englisssssssssh",
-                language2: "Hindi",
-                language3: "Spanish",
-                language4: "French",
-              },
-              references: [
-                {
-                  name: "Jane Smith",
-                  contact: "+1-123-456-7890",
-                },
-                {
-                  name: "Bob Johnson",
-                  contact: "+1-987-654-3210",
-                },
-              ],
-              projects: [
-                {
-                  name: "E-commerce Website",
-                  image: "https://via.placeholder.com/150",
-                },
-                {
-                  name: "Social Media App",
-                  image: "https://via.placeholder.com/150",
-                },
-              ],
-            }}
-          />
+          <UserDetails />
         </>
       )}
     </>
