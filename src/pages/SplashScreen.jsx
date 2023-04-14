4f7a3db780e0f93622cf6c42121f6ce569fdc109
zenitsu <zenitsu@pop-os.localdomain>
2023-04-14 09:35:11 +0530

    changes

--- src/pages/SplashScreen.jsx
+++ src/pages/SplashScreen.jsx
@@ -1,30 +1,20 @@
 import Image from "next/image";
 import React from "react";
-import logo from "../../public/assets/images/logo.png";
-import tie from "../../public/assets/images/tie.png";
+import logo from "../../public/assets/images/logo.gif";
 
 const SplashScreen = () => {
   return (
-    <div className="h-screen bg-gradient-to-bl from-container-blue to-blue-300 w-full relative flex justify-center align-middle">
+    <div className="h-screen bg-white w-full relative flex justify-center align-middle">
       <Image
-        className="animate-splash h-[600px] w-[200px] mt-28 2xl:mt-60 animate-reverse "
+        className="h-[400px] w-[450px] mt-48 2xl:mt-60 "
         src={logo}
         alt="logo"
-        height={500}
       />
-
-      <Image
-        className=" animate-wiggle h-[140px] w-[50px]   absolute 2xl:top-[475px] 2xl:left-[850px] top-[345px] left-[650px]"
-        src={tie}
-        alt="logo"
-        height={270}
-      />
-
-      <div className=" animate-slide absolute w-1 2xl:h-[200px] 2xl:top-[435px]   h-[200px] top-[300px]  bg-gray-600"></div>
-      <p className=" animate-slidetext absolute 2xl:h-[200px] 2xl:top-[435px] 2xl:left-[1100px] 2xl:text-5xl text-2xl left-[800px] top-[350px] font-extrabold font-raleway text-white">
+      <div className=" animate-slide absolute w-1 2xl:h-[200px] 2xl:top-[435px]   h-[200px] top-[300px]  bg-black"></div>
+      <p className=" animate-slidetext absolute 2xl:h-[200px] 2xl:top-[435px] 2xl:left-[1100px] 2xl:text-5xl text-2xl left-[800px] top-[350px] font-extrabold font-raleway text-black">
         Get Hired
       </p>
-      <p className="animate-slidetext absolute 2xl:h-[200px] 2xl:top-[555px] 2xl:left-[1100px] left-[800px] top-[400px] 2xl:text-5xl text-2xl font-extrabold font-raleway text-white">
+      <p className="animate-slidetext absolute 2xl:h-[200px] 2xl:top-[555px] 2xl:left-[1100px] left-[800px] top-[400px] 2xl:text-5xl text-2xl font-extrabold font-raleway text-black">
         Dont Get Fired
       </p>
     </div>
